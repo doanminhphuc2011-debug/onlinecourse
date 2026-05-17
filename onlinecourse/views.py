43c9b868cae9da3ffea7a14017dcf9a047e2a275
doanminhphuc2011-debug <doanminhphuc2011@gmail.com>
2026-05-17 09:13:49 +0700

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import render, redirect
-from .models import Course, Enrollment, Submission
+from .models import Course, Enrollment, Submission, Choice
 
 
 def submit(request, course_id):
     course = Course.objects.get(pk=course_id)
+
     enrollment = Enrollment.objects.first()
+
+    selected_choices = request.POST.getlist('choice')
 
     submission = Submission.objects.create(
         enrollment=enrollment
     )
 
-    return redirect('show_exam_result', course_id=course.id)
+    for choice_id in selected_choices:
+        choice = Choice.objects.get(pk=choice_id)
+        submission.choices.add(choice)
+
+    return redirect(
+        'show_exam_result',
+        course_id=course.id
+    )
 
 
 def show_exam_result(request, course_id):
     course = Course.objects.get(pk=course_id)
 
-    total_score = 85
-    possible_score = 100
+    total_score = 0
+    possible_score = 0
+
+    for question in course.question_set.all():
+        possible_score += question.grade
+
+        selected_ids = []
+
+        if question.is_get_score(selected_ids):
+            total_score += question.grade
 
     context = {
         'course': course,
