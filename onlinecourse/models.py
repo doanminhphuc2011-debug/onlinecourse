d9bc3e0fabb863bc9505287237440ecbca4d0d5f
doanminhphuc2011-debug <doanminhphuc2011@gmail.com>
2026-05-17 08:51:32 +0700

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -24,3 +24,5 @@ class Choice(models.Model):
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+    def __str__(self):
+        return f"Submission {self.id}"
